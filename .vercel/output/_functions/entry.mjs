80b114e50298b0b62112604fca3dc0859495a78f
Enmanoell Mosley <enmamosley@gmail.com>
2025-05-31 23:43:56 -0600

    ajuste node

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,33 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_UE_aKrTU.mjs';
+import { manifest } from './manifest_D2EVVlUQ.mjs';
+
+const serverIslandMap = new Map([
+	['BookScore', () => import('./chunks/BookScore_CaPeozAP.mjs')],
+	['BuyButton', () => import('./chunks/BuyButton_CB5rAsGp.mjs')],
+]);;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/libro/_id_.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/libro/[id].astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "8d53573f-785e-46bd-9bbd-cfc8d679cc87",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
